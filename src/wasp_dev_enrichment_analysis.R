b6abd93700692aff235234782de4d0674154ac93
Sarah <sninwood@gmail.com>
2018-12-04 13:11:18 +1300

    fgsea results plotting

--- src/wasp_dev_enrichment_analysis.R
+++ src/wasp_dev_enrichment_analysis.R
@@ -28,3 +28,19 @@ names(ranks) <- res_group[!is.na(stat), rn]
 fgsea_res <- fgsea(pathways, ranks, nperm = 10000)
 sorted_fgsea_res <- fgsea_res[order(fgsea_res$padj)]
 fwrite(sorted_fgsea_res, "output/fgsea/fgsea_developing_wasp_120vsC.csv")
+
+##read in file with functions added to GO terms when padj<0.1
+annot_fgsea_res <- fread("output/fgsea/annot_fgsea_developing_wasp_120vsC.csv")
+##split into 3 tables --> biological process, cellular component and molecular function
+bp_res <- annot_fgsea_res[annot_fgsea_res$`pathway_kind`=="biological process"]
+cc_res <- annot_fgsea_res[annot_fgsea_res$`pathway_kind`=="cellular component"]
+mf_res <- annot_fgsea_res[annot_fgsea_res$`pathway_kind`=="molecular function"]
+
+##plot normalised enrichment for GO terms where padj<0.1 (but indicate if padj<0.05) - can change to only bp, cc or mf
+ggplot(cc_res, aes(reorder(pathway_name, NES), NES)) +
+  geom_text(aes(label=round(padj, digits=3)), vjust=0, hjust=0) +
+  geom_col(aes(fill=padj<0.05)) +
+  coord_flip() +
+  labs(x="Cellular Component GO Pathway", y="FGSEA Normalized Enrichment Score") + 
+  theme_minimal()
+
